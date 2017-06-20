e3776676a809b9e4fa1fcacfa164ccb0f0551d24
Ryan Greenberg <hawk1138@gmail.com>
2017-06-20 14:43:20 -0700

    finished all functions, all tests pass, and can be run with npm start command

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,49 +1,54 @@
-const week = [
-  'Sun',
-  'Mon',
-  'Tue',
-  'Wed',
-  'Thu',
-  'Fri',
-  'Sat'
-]
+const functions = {
 
-function weekday(date) {
-    if (date.getDay() >= 0 && date.getDay() <= 6) {
-      return week[date.getDay()];
+  weekday(date) {
+    const week = [
+      'Sun',
+      'Mon',
+      'Tue',
+      'Wed',
+      'Thu',
+      'Fri',
+      'Sat'
+    ];
+    if (typeof date !== 'object') {
+      return 'I highly doubt you gave me a proper date.'
+    }
+
+    const day = date.getDay()
+    return week[day];
+  },
+
+  snippet(string, maxLength) {
+    if (maxLength >= string.length) {
+      return string;
+    } else if (maxLength < string.length) {
+      return string.substring(0, maxLength) + '...';
     } else {
-      return "I highly doubt you gave me a proper date.";
+      return "Something has gone horribly wrong!";
     }
-}
+  },
 
-function snippet(string, maxLength) {
-  if (maxLength >= string.length) {
-    return string;
-  } else if (maxLength < string.length) {
-    return string.substring(0, maxLength - 1) + '...';
-  } else {
-    return "Something has gone horribly wrong!";
-  }
-}
+  numProps(obj) {
+    if (typeof obj === 'object') {
+      return Object.keys(obj).length;
+    } else {
+      return "I'm not even sure you handed in an object.";
+    }
+  },
 
-function numProps(obj) {
-  if (typeof obj === 'object') {
-    return Object.keys(obj).length;
-  } else {
-    return "I'm not even sure you handed in an object.";
-  }
-}
-
-function filterBetween(array, min, max) {
-  var answer = [];
-  if (typeof array !== 'array' || typeof min !== 'number' || typeof max !== 'number') {
-    return 'invalid input attempt';
-  } else {
-    for (var i = 0; i < array.length; i++ ) {
-      if (array[i] <= max && array[i] >= min) {
-        answer.push(array[i]);
+  filterBetween(array, min, max) {
+    var answer = [];
+    if (typeof array !== 'object' || typeof min !== 'number' || typeof max !== 'number') {
+      return 'invalid input attempt';
+    } else {
+      for (var i = 0; i < array.length; i++ ) {
+        if (array[i] <= max && array[i] >= min) {
+          answer.push(array[i]);
+        }
       }
+      return answer;
     }
-    return answer;
   }
 }
+
+module.exports = functions;
